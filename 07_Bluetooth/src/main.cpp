834645eaac1296d2449fa3d727d5bab79f1a8a8c
philippeSimier <philaure@wanadoo.fr>
2024-01-24 13:21:00 +0100

    add Afficheur OLED

--- 07_Bluetooth/src/main.cpp
+++ 07_Bluetooth/src/main.cpp
@@ -6,32 +6,40 @@
  */
 
 #include <BluetoothSerial.h>
+#include <Afficheur.h>
 
 BluetoothSerial SerialBT;
+Afficheur afficheur;
 
 void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("CIEL_BT"); // Le nom du périphérique Bluetooth
+    Serial.begin(115200);
+    afficheur.init();
+    SerialBT.begin("CIEL_BT1"); // Le nom du périphérique Bluetooth
 
-  Serial.println("Le périphérique est prêt à être apparié.");
+    Serial.println("Le périphérique est prêt à être apparié.");
+    afficheur.afficher("Bluetooth");
 }
 
 void loop() {
-  if (SerialBT.available()) {
-    // Lire les données Bluetooth
-    char incomingByte = SerialBT.read();
-    Serial.print("Donnée reçue: ");
-    Serial.println(incomingByte);
-  }
 
-  // Envoyer des données Bluetooth
-  if (Serial.available()) {
-    char outgoingByte = Serial.read();
-    SerialBT.print("Donnée envoyée: ");
-    SerialBT.println(outgoingByte);
-  }
+    // Lire les données Bluetooth pour les afficher
+    char incomingByte;
+    while (SerialBT.available()) {
+        
+        incomingByte = SerialBT.read();
+        Serial.write(incomingByte);
+        afficheur.afficher(incomingByte);
+    }
 
-  delay(20);
+    // Envoyer des données Bluetooth
+    char outgoingByte;
+    
+    while (Serial.available()) {
+        outgoingByte = Serial.read();
+        SerialBT.write(outgoingByte);
+    }
+
+    delay(20);
 }
 
 
